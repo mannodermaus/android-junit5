00434a6ea20d2e8cc76dadd8b276562360f3762d
Marcel Schnelle <ms@planetmutlu.com>
2017-07-08 14:56:20 +0900

    Fixed Travis deployment of snapshots by falling back to sysenv correctly

--- scripts/deploy_snapshot.sh
+++ scripts/deploy_snapshot.sh
@@ -21,6 +21,6 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
   echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
 else
   echo "Deploying snapshot..."
-  ./gradlew publishLibraryPublicationToSnapshotRepository
+  ./gradlew publishLibraryPublicationToSnapshotRepository --info --stacktrace
   echo "Snapshot deployed!"
 fi
