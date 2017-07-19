d862519c059640352215e9d530fd0d1872955dfb
Marcel Schnelle <aurae@web.de>
2017-07-19 14:47:07 +0900

    Added Kotlin sample class & modified Run Configurations

--- sample/src/test/java/ExampleJavaTest.java
+++ sample/src/test/java/ExampleJavaTest.java
@@ -1,10 +1,28 @@
-package de.mannodermaus.gradle.plugins.android_junit5.sample;
-
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static java.util.Collections.unmodifiableList;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 final class ExampleJavaTest {
 
@@ -51,6 +69,7 @@ final class ExampleJavaTest {
      * Assert.assertXXX                      -> Assertions.assertXXX
      * n/a                                   -> @DisplayName
      * n/a                                   -> @Nested
+     * n/a                                   -> @TestFactory
      */
 
     @Test
@@ -88,5 +107,17 @@ final class ExampleJavaTest {
         void testInNestedClass() {
             assertEquals("LOL", "LO" + "L");
         }
+    }
+
+    @TestFactory
+    Collection<DynamicTest> dynamicTestsGeneratedFromFactory() {
+        String input = "Dynamic Test Input";
+
+        List<DynamicTest> tests = new ArrayList<>();
+        tests.add(dynamicTest("Length of input", () -> assertEquals(18, input.length())));
+        tests.add(dynamicTest("Lowercase of input", () -> assertEquals("dynamic test input", input.toLowerCase())));
+        tests.add(dynamicTest("Uppercase of input", () -> assertEquals("DYNAMIC TEST INPUT", input.toUpperCase())));
+
+        return unmodifiableList(tests);
     }
 }
