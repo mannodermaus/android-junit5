5c794231c42398b63f31fb35f1bf22a98b5d3130
Marcel Schnelle <aurae@web.de>
2018-03-03 23:37:40 +0900

    Gracefully check for JUnitPlatform before AndroidJUnit5 to avoid NoClassDefFoundError
    Also, add thorough documentation on how these conditional runtime checks work.

--- instrumentation-runner/src/main/kotlin/de/mannodermaus/junit5/RunnerBuilder.kt
+++ instrumentation-runner/src/main/kotlin/de/mannodermaus/junit5/RunnerBuilder.kt
@@ -33,7 +33,22 @@ class AndroidJUnit5Builder : RunnerBuilder() {
 
   private val junit5Available by lazy {
     try {
+      // The verification order of this block is quite important.
+      // Do not change it without thorough testing of potential consequences!
+      // After tampering with this, verify that integration
+      // with applications using JUnit 5 for UI tests still works,
+      // AND that integration with applications NOT using JUnit 5 for UI tests still works.
+      //
+      // First, verify the existence of junit-jupiter-api on the classpath.
+      // Then, verify that the JUnitPlatform Runner is available on the classpath.
+      // It is VERY important to perform this check BEFORE verifying the existence
+      // of the AndroidJUnit5 Runner, which inherits from JUnitPlatform. If this is omitted,
+      // an uncatchable verification error will be raised, rendering instrumentation testing
+      // for applications without the desire to include JUnit 5 effectively useless.
+      // The simple Class.forName() check however will catch this allowed inconsistency,
+      // and gracefully abort.
       Class.forName("org.junit.jupiter.api.Test")
+      Class.forName("org.junit.platform.runner.JUnitPlatform")
       Class.forName("de.mannodermaus.junit5.AndroidJUnit5")
       true
     } catch (e: Throwable) {
