6eb0419fee4d77d2d640eedaacbfee63f3515a76
Marcel Schnelle <marcel@mytenten.com>
2018-09-02 18:49:40 +0200

    Fix multi-dimension projects that try to access non-existing filters (#113)
    Fixes #110

--- android-junit5/src/main/kotlin/de/mannodermaus/gradle/plugins/junit5/Configuration.kt
+++ android-junit5/src/main/kotlin/de/mannodermaus/gradle/plugins/junit5/Configuration.kt
@@ -94,16 +94,33 @@ internal class JUnit5TaskConfig(
   // 2) Build-type-specific (e.g. "debug")
   // 3) Flavor-specific (e.g. "free")
   // 4) Variant-specific (e.g. "freeDebug")
-  private fun collect(collectorFunction: (FiltersExtension) -> IncludeExcludeContainer) =
-      collectorFunction(extension.filters) +
-          collectorFunction(extension.findFilters(variant.buildType.name)) +
-          collectorFunction(extension.findFilters(variant.flavorName)) +
-          collectorFunction(extension.findFilters(variant.name))
+  private fun collect(
+      func: FiltersExtension.() -> IncludeExcludeContainer): IncludeExcludeContainer {
+    // 1)
+    val layer1 = filtersOf(null, func)
+    // 2)
+    val layer2 = layer1 + filtersOf(variant.buildType.name, func)
+    // 3)
+    val layer3 = variant.productFlavors
+        .map { filtersOf(it.name, func) }
+        .fold(layer2) { a, b -> a + b }
+    // 4)
+    return layer3 + filtersOf(variant.name, func)
+  }
 
-  val combinedIncludePatterns = this.collect { it.patterns }.include.toTypedArray()
-  val combinedExcludePatterns = this.collect { it.patterns }.exclude.toTypedArray()
-  val combinedIncludeTags = this.collect { it.tags }.include.toTypedArray()
-  val combinedExcludeTags = this.collect { it.tags }.exclude.toTypedArray()
-  val combinedIncludeEngines = this.collect { it.engines }.include.toTypedArray()
-  val combinedExcludeEngines = this.collect { it.engines }.exclude.toTypedArray()
+  private inline fun filtersOf(
+      qualifier: String?,
+      func: FiltersExtension.() -> IncludeExcludeContainer) =
+      if (qualifier == null) {
+        extension.filters.func()
+      } else {
+        extension.findFilters(qualifier).func()
+      }
+
+  val combinedIncludePatterns = this.collect { patterns }.include.toTypedArray()
+  val combinedExcludePatterns = this.collect { patterns }.exclude.toTypedArray()
+  val combinedIncludeTags = this.collect { tags }.include.toTypedArray()
+  val combinedExcludeTags = this.collect { tags }.exclude.toTypedArray()
+  val combinedIncludeEngines = this.collect { engines }.include.toTypedArray()
+  val combinedExcludeEngines = this.collect { engines }.exclude.toTypedArray()
 }
