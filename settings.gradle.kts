230e158b1ff2649fd2faa4209bff7a8d78229588
Marcel <aurae@web.de>
2018-11-04 14:22:56 +0100

    Update settings.gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "android-junit5-root"
+
+include(":android-junit5")
+include(":android-junit5-tests")
+include(":sample")
+include(":instrumentation")
+include(":instrumentation-runner")
