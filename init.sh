08fa91e6201427d91a6529395058a5ee099e2198
Marcel <aurae@web.de>
2019-03-16 21:39:54 +0100

    Add init script to test out initial-setups

---
+++ init.sh
@@ -0,0 +1,13 @@
+cd instrumentation
+ln -s ../buildSrc buildSrc
+ln -s ../gradle gradle
+ln -s ../build.gradle.kts build.gradle.kts
+ln -s ../local.properties local.properties
+cd -
+
+cd plugin
+ln -s ../buildSrc buildSrc
+ln -s ../gradle gradle
+ln -s ../build.gradle.kts build.gradle.kts
+ln -s ../local.properties local.properties
+cd -
