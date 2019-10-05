888f618f9dd45a09e1ac512c19d6eb3d31663adb
Marcel <aurae@web.de>
2019-10-05 15:36:36 +0200

    Bump plugin version

--- buildSrc/src/main/kotlin/Artifacts.kt
+++ buildSrc/src/main/kotlin/Artifacts.kt
@@ -35,7 +35,7 @@ object Artifacts {
       platform = Java,
       groupId = "de.mannodermaus.gradle.plugins",
       artifactId = "android-junit5",
-      currentVersion = "1.5.2.0-SNAPSHOT",
+      currentVersion = "1.5.2.0",
       latestStableVersion = "1.5.1.0",
       license = license,
       description = "Unit Testing with JUnit 5 for Android."
