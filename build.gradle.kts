da10df99dbfee2c06ab0f7d1b98dec06de67b611
Marcel <aurae@web.de>
2019-10-05 15:29:40 +0200

    Autofind root folder

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,19 @@ allprojects {
 }
 
 tasks.create<GenerateReadme>("generateReadme") {
-  inputTemplateFile = project.file("README.md.template")
-  outputFile = project.file("README.md")
+  // Find folder containing README.md
+  // (required because this script file is included through symlinks in sub-projects)
+  var rootFolder = project.rootDir
+  while (rootFolder != null && rootFolder.exists()) {
+    val inFile = File(rootFolder, "README.md.template")
+    val outFile = File(rootFolder, "README.md")
+
+    if (inFile.exists() && outFile.exists()) {
+      this.inputTemplateFile = inFile
+      this.outputFile = outFile
+      break
+    }
+    
+    rootFolder = rootFolder.parentFile
+  }
 }
