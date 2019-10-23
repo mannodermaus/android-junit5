0b7f5b4a20b37e4a7b5877ad880ddb6bdac25203
Marcel <aurae@web.de>
2019-10-23 22:14:11 +0200

    Revamp FunctionalTests to be less repetitive
    Use a common DSL to assert each test project's output, instead of
    having to assert the BuildResult manually over and over again.
    There is still a case-by-case switch because the assertions are different
    for each AGP version, but this variant is still much improved, and has
    less code duplication.

--- plugin/android-junit5/src/test/kotlin/de/mannodermaus/gradle/plugins/junit5/util/AgpVersion.kt
+++ plugin/android-junit5/src/test/kotlin/de/mannodermaus/gradle/plugins/junit5/util/AgpVersion.kt
@@ -8,6 +8,13 @@ enum class AgpVersion(val fileKey: String) {
   AGP_36X("agp36x"),
   AGP_40X("agp40x");
 
+  // Create a pretty string from the fileKey property.
+  // Example:
+  // fileKey    = "agp32x"
+  // prettyName = "3.2"
+  val prettyName: String = fileKey.substring(3, 5)
+      .let { v -> "${v[0]}.${v[1]}" }
+
   companion object {
     fun latest() = AGP_34X
   }
