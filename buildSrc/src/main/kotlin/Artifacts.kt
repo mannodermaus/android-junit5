ef75d003716dd79a1b28da629168aa085bb794c2
Marcel <aurae@web.de>
2020-02-28 06:42:45 +0100

    Prepare next version

--- buildSrc/src/main/kotlin/Artifacts.kt
+++ buildSrc/src/main/kotlin/Artifacts.kt
@@ -35,8 +35,8 @@ object Artifacts {
       platform = Java,
       groupId = "de.mannodermaus.gradle.plugins",
       artifactId = "android-junit5",
-      currentVersion = "1.6.0.0",
-      latestStableVersion = "1.5.2.0",
+      currentVersion = "1.6.1.0-SNAPSHOT",
+      latestStableVersion = "1.6.0.0",
       license = license,
       description = "Unit Testing with JUnit 5 for Android."
   )
