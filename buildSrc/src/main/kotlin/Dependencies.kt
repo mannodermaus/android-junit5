0556ada7ac45f55094354082f05f4d44cc27e70c
Marcel <aurae@web.de>
2020-06-18 22:52:58 +0200

    AGP 4.2-alpha2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -18,7 +18,7 @@ object Plugins {
   val android36x: Agp = Agp("com.android.tools.build:gradle:3.6.3")
   val android40x: Agp = Agp("com.android.tools.build:gradle:4.0.0")
   val android41x: Agp = Agp("com.android.tools.build:gradle:4.1.0-beta01", requiresGradle = "6.5-rc-1")
-  val android42x: Agp = Agp("com.android.tools.build:gradle:4.2.0-alpha01", requiresGradle = "6.5")
+  val android42x: Agp = Agp("com.android.tools.build:gradle:4.2.0-alpha02", requiresGradle = "6.5")
   val android: Agp = android35x
 
   val supportedAndroidPlugins = listOf(
