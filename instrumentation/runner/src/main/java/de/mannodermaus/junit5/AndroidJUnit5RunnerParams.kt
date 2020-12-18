48d25a842a6831d7aae09660f95cb31735c10d89
Marcel <marcelschnelle@aol.com>
2020-12-18 14:03:34 +0100

    Provide configuration parameters to instrumentation tests
    Include them in the discovery request created by the JUnit 5 Runner.
    This allows auto-detection of extensions, for example. Reuse the same
    mechanism for parsing as exercised for system properties & env vars,
    using the PropertiesParser to split a comma-separated string of key-value pairs.

--- instrumentation/runner/src/main/java/de/mannodermaus/junit5/AndroidJUnit5RunnerParams.kt
+++ instrumentation/runner/src/main/java/de/mannodermaus/junit5/AndroidJUnit5RunnerParams.kt
@@ -9,12 +9,14 @@ data class AndroidJUnit5RunnerParams(
     private val selectors: List<DiscoverySelector> = emptyList(),
     private val filters: List<Filter<*>> = emptyList(),
     val environmentVariables: Map<String, String> = emptyMap(),
-    val systemProperties: Map<String, String> = emptyMap()
+    val systemProperties: Map<String, String> = emptyMap(),
+    private val configurationParameters: Map<String, String> = emptyMap()
 ) {
 
   fun createDiscoveryRequest(): LauncherDiscoveryRequest =
       LauncherDiscoveryRequestBuilder.request()
           .selectors(this.selectors)
           .filters(*this.filters.toTypedArray())
+          .configurationParameters(this.configurationParameters)
           .build()
 }
