0ebd4fa7c37a413788ca4625cf153fe59b1797ab
Marcel Schnelle <marcelschnelle@aol.com>
2021-08-18 15:04:36 +0200

    AGP 7.1.0 Alpha 8

--- build-logic/src/main/kotlin/Environment.kt
+++ build-logic/src/main/kotlin/Environment.kt
@@ -12,7 +12,7 @@ enum class SupportedAgp(
     AGP_4_1("4.1.2", gradle = "6.5"),
     AGP_4_2("4.2.2", gradle = "6.7.1"),
     AGP_7_0("7.0.0", gradle = "7.0.2"),
-    AGP_7_1("7.1.0-alpha07", gradle = "7.1");
+    AGP_7_1("7.1.0-alpha08", gradle = "7.1");
 
     val shortVersion: String = run {
         // Extract first two components of the Maven dependency's version string.
