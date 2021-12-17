080159b037ad336cfb9902395563448fc4554245
Marcel Schnelle <marcelschnelle@aol.com>
2021-12-17 21:13:29 +0100

    AGP 7.1 Beta 5 & 7.2 Alpha 6

--- build-logic/src/main/kotlin/Environment.kt
+++ build-logic/src/main/kotlin/Environment.kt
@@ -12,8 +12,8 @@ enum class SupportedAgp(
     AGP_4_1("4.1.2", gradle = "6.5"),
     AGP_4_2("4.2.2", gradle = "6.7.1"),
     AGP_7_0("7.0.4", gradle = "7.0.2"),
-    AGP_7_1("7.1.0-beta04", gradle = "7.2"),
-    AGP_7_2("7.2.0-alpha05", gradle = "7.3-rc-1");
+    AGP_7_1("7.1.0-beta05", gradle = "7.2"),
+    AGP_7_2("7.2.0-alpha06", gradle = "7.3");
 
     val shortVersion: String = run {
         // Extract first two components of the Maven dependency's version string.
