01e53983b9d949ea91f6c8385061c188ffce1753
Marcel Schnelle <marcelschnelle@aol.com>
2022-09-20 22:28:00 +0200

    AGP 7.4 Beta 1 & AGP 8.0 Alpha 1

--- build-logic/src/main/kotlin/Environment.kt
+++ build-logic/src/main/kotlin/Environment.kt
@@ -12,7 +12,8 @@ enum class SupportedAgp(
     AGP_7_1("7.1.3", gradle = "7.2"),
     AGP_7_2("7.2.1", gradle = "7.3.3"),
     AGP_7_3("7.3.0", gradle = "7.4.2"),
-    AGP_7_4("7.4.0-alpha10", gradle = "7.5.1");
+    AGP_7_4("7.4.0-beta01", gradle = "7.5.1"),
+    AGP_8_0("8.0.0-alpha01", gradle = "7.5.1");
 
     companion object {
         val oldest = values().first()
