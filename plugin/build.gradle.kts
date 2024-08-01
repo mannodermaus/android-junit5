fba6585e8e6073f979d40cd3f65ed101550f886b
Marcel Schnelle <marcelschnelle@aol.com>
2024-08-01 09:49:53 +0900

    Change readme generation task to abstract

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -39,7 +39,7 @@ apiValidation {
 
 tasks.create<GenerateReadme>("generateReadme") {
     // Find folder containing README.md
-    // (required because this script file is included through symlinks in sub-projects)
+    // (required because this script file is included through symlinks in subprojects)
     var rootFolder: File? = project.rootDir
     while (rootFolder != null && rootFolder.exists()) {
         val inFile = File(rootFolder, "README.md.template")
