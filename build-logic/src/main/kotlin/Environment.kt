6a874bc36f18898ae261a183eeb5a175de8a2e76
Marcel Schnelle <marcelschnelle@aol.com>
2024-09-02 18:45:02 +0900

    AGP 8.6, 8.7 Canary 9

--- build-logic/src/main/kotlin/Environment.kt
+++ build-logic/src/main/kotlin/Environment.kt
@@ -14,8 +14,8 @@ enum class SupportedAgp(
     AGP_8_3("8.3.2", gradle = "8.4"),
     AGP_8_4("8.4.2", gradle = "8.6"),
     AGP_8_5("8.5.2", gradle = "8.7"),
-    AGP_8_6("8.6.0-rc01", gradle = "8.7"),
-    AGP_8_7("8.7.0-alpha07", gradle = "8.9");
+    AGP_8_6("8.6.0", gradle = "8.7"),
+    AGP_8_7("8.7.0-alpha09", gradle = "8.9");
 
     companion object {
         val oldest = values().first()
