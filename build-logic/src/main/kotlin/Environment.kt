992d3da8d9523bd7a95eae7bc291790c2715fd1c
Marcel Schnelle <marcelschnelle@aol.com>
2025-03-15 11:30:22 +0900

    Bump snapshots

--- build-logic/src/main/kotlin/Environment.kt
+++ build-logic/src/main/kotlin/Environment.kt
@@ -91,7 +91,7 @@ object Artifacts {
         platform = Java,
         groupId = "de.mannodermaus.gradle.plugins",
         artifactId = "android-junit5",
-        currentVersion = "1.12.0.1-SNAPSHOT",
+        currentVersion = "1.12.1.0-SNAPSHOT",
         latestStableVersion = "1.12.0.0",
         description = "Unit Testing with JUnit 5 for Android."
     )
@@ -101,7 +101,7 @@ object Artifacts {
      */
     object Instrumentation {
         const val groupId = "de.mannodermaus.junit5"
-        private const val currentVersion = "1.7.1-SNAPSHOT"
+        private const val currentVersion = "1.8.0-SNAPSHOT"
         private const val latestStableVersion = "1.7.0"
 
         val Core = Deployed(
