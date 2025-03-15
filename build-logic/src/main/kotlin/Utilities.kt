3e1bd38b6d82c4bc04fcd57cd09ce7d4e31c1e9b
Marcel Schnelle <marcelschnelle@aol.com>
2025-03-15 10:56:16 +0900

    Clean unused utility functions

--- build-logic/src/main/kotlin/Utilities.kt
+++ build-logic/src/main/kotlin/Utilities.kt
@@ -1,7 +1,5 @@
-import org.gradle.api.Action
 import org.gradle.api.Project
 import org.gradle.api.artifacts.dsl.RepositoryHandler
-import org.gradle.api.artifacts.repositories.MavenArtifactRepository
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.tasks.compile.AbstractCompile
 import org.gradle.kotlin.dsl.withGroovyBuilder
@@ -12,32 +10,26 @@ import java.time.format.DateTimeFormatter
 
 /* RepositoryHandler */
 
-fun RepositoryHandler.jitpack() =
-    this.maven(object: Action<MavenArtifactRepository> {
-      override fun execute(repo: MavenArtifactRepository) {
-        repo.setUrl("https://jitpack.io")
-      }
-    })
+fun RepositoryHandler.jitpack() = maven {
+    setUrl("https://jitpack.io")
+}
 
-fun RepositoryHandler.sonatypeSnapshots() =
-    this.maven(object: Action<MavenArtifactRepository> {
-      override fun execute(repo: MavenArtifactRepository) {
-        repo.setUrl("https://oss.sonatype.org/content/repositories/snapshots")
-      }
-    })
+fun RepositoryHandler.sonatypeSnapshots() = maven {
+    setUrl("https://oss.sonatype.org/content/repositories/snapshots")
+}
 
 /* Project */
 
 fun Project.fixCompileTaskChain() {
-  setupCompileChain(
-      sourceCompileName = "compileKotlin",
-      targetCompileName = "compileGroovy"
-  )
+    setupCompileChain(
+        sourceCompileName = "compileKotlin",
+        targetCompileName = "compileGroovy"
+    )
 
-  setupCompileChain(
-      sourceCompileName = "compileTestKotlin",
-      targetCompileName = "compileTestGroovy"
-  )
+    setupCompileChain(
+        sourceCompileName = "compileTestKotlin",
+        targetCompileName = "compileTestGroovy"
+    )
 }
 
 /**
@@ -48,41 +40,12 @@ private fun Project.setupCompileChain(
     sourceCompileName: String,
     targetCompileName: String
 ) {
-  val targetCompile = tasks.getByName(targetCompileName) as AbstractCompile
-  val sourceCompile = tasks.getByName(sourceCompileName)
+    val targetCompile = tasks.getByName(targetCompileName) as AbstractCompile
+    val sourceCompile = tasks.getByName(sourceCompileName)
 
-  // Allow calling the source language's classes from the target language.
-  // In this case, we allow calling Kotlin from Groovy - it has to be noted however,
-  // that the other way does not work!
-  val sourceDir = sourceCompile.withGroovyBuilder { getProperty("destinationDirectory") } as DirectoryProperty
-  targetCompile.classpath += project.files(sourceDir.get().asFile)
+    // Allow calling the source language's classes from the target language.
+    // In this case, we allow calling Kotlin from Groovy - it has to be noted however,
+    // that the other way does not work!
+    val sourceDir = sourceCompile.withGroovyBuilder { getProperty("destinationDirectory") } as DirectoryProperty
+    targetCompile.classpath += project.files(sourceDir.get().asFile)
 }
-
-/**
- * Provides a dependency object to the JUnit 5 plugin, if any can be found.
- * This will look in the build folder of the sibling project to try and find
- * a previously built "fat JAR", and return it in a format
- * compatible to the Gradle dependency mechanism. If no file can be found,
- * this method returns null instead.
- */
-fun Project.findLocalPluginJar(): File? {
-  val localLibsFolder = rootDir.parentFile.toPath()
-      .resolve("plugin/android-junit5/build/libs")
-      .toFile()
-
-  val localPluginJar = (localLibsFolder.listFiles() ?: emptyArray<File>())
-      .sortedByDescending(File::lastModified)
-      .firstOrNull { "fat" in it.name && "javadoc" !in it.name && "sources" !in it.name }
-
-  return localPluginJar
-}
-
-/* File */
-
-/**
- * Format the "last modified" timestamp of a file into a human readable string.
- */
-fun File.lastModifiedDate(): String =
-    Instant.ofEpochMilli(lastModified())
-        .atZone(ZoneId.systemDefault())
-        .format(DateTimeFormatter.ISO_DATE_TIME)
