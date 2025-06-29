0ee7b2f5987e57e2fc2939e6e366454042a196bf
Marcel Schnelle <marcelschnelle@aol.com>
2025-06-29 09:36:52 +0900

    Plugin 1.13.1.0

--- build-logic/src/main/kotlin/Environment.kt
+++ build-logic/src/main/kotlin/Environment.kt
@@ -91,7 +91,7 @@ object Artifacts {
         platform = Java,
         groupId = "de.mannodermaus.gradle.plugins",
         artifactId = "android-junit5",
-        currentVersion = "1.13.1.0-SNAPSHOT",
+        currentVersion = "1.13.1.0",
         latestStableVersion = "1.13.0.0",
         description = "Unit Testing with JUnit 5 for Android."
     )
