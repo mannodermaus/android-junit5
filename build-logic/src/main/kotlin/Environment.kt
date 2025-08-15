50f5e27aeb314eb4a4d6a99472b417714ce6054e
Marcel Schnelle <marcelschnelle@aol.com>
2025-08-15 23:45:48 +0900

    AGP 8.12, 8.13 RC1, 9.0 Alpha 1
    Also update the build script of our test projects to address deprecations and, more importantly, deprecated APIs that were removed in AGP 9.0

--- build-logic/src/main/kotlin/Environment.kt
+++ build-logic/src/main/kotlin/Environment.kt
@@ -19,8 +19,9 @@ enum class SupportedAgp(
     AGP_8_9("8.9.3", gradle = "8.11.1"),
     AGP_8_10("8.10.1", gradle = "8.11.1"),
     AGP_8_11("8.11.1", gradle = "8.13"),
-    AGP_8_12("8.12.0-rc01", gradle = "8.13"),
-    AGP_8_13("8.13.0-alpha02", gradle = "8.13")
+    AGP_8_12("8.12.0", gradle = "8.13"),
+    AGP_8_13("8.13.0-rc01", gradle = "8.13"),
+    AGP_9_0("9.0.0-alpha01", gradle = "9.0.0"),
     ;
 
     companion object {
