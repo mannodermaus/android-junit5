795468e8e000d15466b19e45709406e139fbb534
Marcel Schnelle <marcelschnelle@aol.com>
2025-10-10 23:32:36 +0900

    Plugin 1.14.0.0

--- build-logic/src/main/kotlin/Environment.kt
+++ build-logic/src/main/kotlin/Environment.kt
@@ -93,7 +93,7 @@ object Artifacts {
         platform = Java,
         groupId = "de.mannodermaus.gradle.plugins",
         artifactId = "android-junit5",
-        currentVersion = "1.14.0.0-SNAPSHOT",
+        currentVersion = "1.14.0.0",
         latestStableVersion = "1.13.4.0",
         description = "Unit Testing with JUnit 5 for Android."
     )
@@ -103,8 +103,8 @@ object Artifacts {
      */
     object Instrumentation {
         const val groupId = "de.mannodermaus.junit5"
-        private const val currentVersion = "1.9.0"
-        private const val latestStableVersion = "1.8.0"
+        private const val currentVersion = "1.9.1-SNAPSHOT"
+        private const val latestStableVersion = "1.9.0"
 
         val Core = Deployed(
             platform = Android(minSdk = 19),
